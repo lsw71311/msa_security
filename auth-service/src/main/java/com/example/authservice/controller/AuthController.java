e92a43c446d5aae101a0a23012d1564c29c5da7a
lsw71311 <lsw71311@naver.com>
2024-09-11 02:28:15 +0900

    Init commit

---
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,65 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.config.security.JwtUtil;
+import com.example.authservice.controller.dto.LoginRequestDto;
+import com.example.authservice.service.RedisService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final RedisService redisService;
+    private final JwtUtil jwtTokenUtil;
+
+    @PostMapping("/login")
+    public String login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
+        // TODO: 인증 서버 로그인 시, 토큰 발급 처리
+
+        // 사용자 인증 생성
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password())
+        );
+
+        // 인증 성공 시, Redis 저장을 위한 처리를 위해 User를 가져옴.
+        User user = (User) authentication.getPrincipal();
+
+        // redis에 저장하기 위한 중간 dto.
+        var userDto = UserDto.fromUser(user);
+
+        // redis에 저장될 때 user: 라는 prefix와 username을 합쳐 키를 만들고 userDto를 직렬화하여 저장.
+
+        redisService.setValue("user:" + loginRequestDto.username(), userDto);
+
+        // JWT 토큰 생성 후 리턴
+
+        return jwtTokenUtil.createToken(user.getUsername(), user.getAuthorities());
+
+    }
+
+    private record UserDto(String userName, Collection<String> roles) {
+        public static UserDto fromUser(User user) {
+            return new UserDto(
+                    user.getUsername(),
+                    user.getAuthorities().stream()
+                            .map(GrantedAuthority::getAuthority)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+}
